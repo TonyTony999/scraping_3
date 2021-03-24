d40a9a526fab4bd8f15c0a5ae425f97f82bb8d6d
Tony Jerry <tony@jerry.com>
2021-03-24 13:11:33 -0500

    added .sh

---
+++ new_2.sh
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/proy/shell_scripts:/
+export NODE_PATH=/usr/local/lib/node_modules
+
+cd /home/ubuntu/proy/scraping_3
+
+/usr/bin/node /home/ubuntu/proy/scraping_3/index_3.js >> /home/ubuntu/proy/scraping_3/mylogs.log
